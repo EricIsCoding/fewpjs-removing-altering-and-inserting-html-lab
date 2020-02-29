16dda01f935d6e50507400f462a6a799c3dd78ee
Eric J Wilhite <EricJWilhite@gmail.com>
2020-02-29 00:35:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let main = document.getElementById("main")
+main.remove()
+
+let body = document.querySelector("body")
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Eric is the champion"
